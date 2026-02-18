3f4a2680fffa2d38c1c3cd79da33b346c2ccfbdc
ahstop1112@hotmail.com <ahstop1112@hotmail.com>
2026-02-18 11:06:55 -0800

    delete

--- apps/src/context/auth/useAuth.ts
+++
@@ -1,9 +0,0 @@
-import React, { useContext } from "react";
-import { AuthContext } from "./AuthContext";
-import { AuthContextValue } from "./types";
-
-export const useAuth = (): AuthContextValue => {
-  const ctx = useContext(AuthContext);
-  if (!ctx) throw new Error("useAuth must be used within AuthProvider");
-  return ctx;
-};
